656d80b96e080dd65a3aa3427cda467c85227124
tiagoforward <devtiagoforward@gmail.com>
2024-09-22 10:04:17 -0300

    installed phosphor-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Heart } from "@phosphor-icons/react/dist/ssr";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Heart weight="duotone" />
+    </>
   );
 }
