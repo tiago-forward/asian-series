85de832a95152fe5743ed0dee6038719cead849e
tiagoforward <devtiagoforward@gmail.com>
2024-09-22 08:44:03 -0300

    project creation in nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'blue-100': "var(--background)",
+        'blue-200': "var(--foreground)",
+        'blue-300': "var(--foreground)",
+
+        'rose-100':  "var(--rose-100)",
+        'rose-200':  "var(--rose-200)",
+
+        'neutral-100': "var(--neutral-100)",
+        'neutral-200': "var(--neutral-200)",
+        'neutral-300': "var(--neutral-300)",
+        'neutral-400': "var(--neutral-400)",
+        'neutral-500': "var(--neutral-500)",
+        'neutral-600': "var(--neutral-600)",
+        'neutral-700': "var(--neutral-700)",
+        'neutral-800': "var(--neutral-800)",
+
+        'gradient-horizontal': "var(--gradient-horizontal)",
+        'gradient-vertical': "var(--gradient-vertical)",
       },
     },
   },
